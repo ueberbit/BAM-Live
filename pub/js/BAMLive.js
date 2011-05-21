a41733d788b6f97e595d40b708114b83f54fd15f
Tim Weber <t.weber@digi-info.de>
2011-05-21 20:39:55 +0200

    Add BAMLive.{dbg,dumpConfig,log}()

--- pub/js/BAMLive.js
+++ pub/js/BAMLive.js
@@ -1,5 +1,18 @@
 BAMLive = (function () {
 	var CONFIG = {};
+	var dbg = function () {
+		if (CONFIG.debug) {
+			log.apply(log, arguments);
+		}
+	};
+	var dumpConfig = function () {
+		log(CONFIG);
+	};
+	var log = function () {
+		if (console && console.log) {
+			console.log.apply(console.log, arguments);
+		}
+	};
 	var set = function (options) {
 		for (var key in options) {
 			if (options.hasOwnProperty(key)) {
@@ -8,6 +21,9 @@ BAMLive = (function () {
 		}
 	};
 	return {
+		dbg: dbg,
+		dumpConfig: dumpConfig,
+		log: log,
 		set: set
 	};
 })();
