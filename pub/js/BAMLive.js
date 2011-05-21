24dc207fdbbc590493a3ddf6b35d1282ba81eeab
Tim Weber <t.weber@digi-info.de>
2011-05-21 20:30:13 +0200

    Add config storage and BAMLive.set()

--- pub/js/BAMLive.js
+++ pub/js/BAMLive.js
@@ -1,4 +1,13 @@
 BAMLive = (function () {
+	var CONFIG = {};
+	var set = function (options) {
+		for (var key in options) {
+			if (options.hasOwnProperty(key)) {
+				CONFIG[key] = options[key];
+			}
+		}
+	};
 	return {
+		set: set
 	};
 })();
